44c31648ded0cd09426ecc5662b7e9623fbf3bea
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-16 13:07:40 -0300

    atendimento.cpp

--- atendimento.cpp
+++ atendimento.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>
+
 //Multi-Process Handling
 #include <pthread.h>
 
@@ -13,7 +13,7 @@
 
 //Time
 #include <sys/time.h>
-
+// Client struct
 struct{
 	pid_t pid;
 	// Arrive time in u seconds (10^{-6})
@@ -22,6 +22,12 @@ struct{
 	// wait time for service
 	double wTime;
 }typedef Client;
+
+//Arg struct to pthread
+struct reception_struct {
+	int n;
+	FILE* file;
+};
 
 sem_t* sem_atend;
 sem_t* sem_block;
@@ -33,15 +39,17 @@ int randomPriority (){
 void *stop (void* args){
 	int i = 0;
 	while(getchar()!='s')
-		i++;
+		i = i+1;
 	exit(0);
+	return args;
 }
 
 
 void *service (void* arg){
-
+	return nullptr;
 }
-void *reception (void *args){
+void *reception (void *arguments){
+	struct  reception_struct *args = (struct reception_struct *)arguments;
 	sem_atend = sem_open("/sem_atend", O_CREAT | O_EXCL, 0644, 1);
 	if (sem_atend == SEM_FAILED) {
 		sem_unlink("/sem_atend");
@@ -52,33 +60,32 @@ void *reception (void *args){
 		sem_unlink("/sem_block");
 		sem_block = sem_open("/sem_block", O_CREAT | O_EXCL, 0644, 1);
 	}
-
-
+	std::cout << "Processes: " + args->n << '\n';
+	exit(0);
+	return nullptr;
 }
-struct reception_struct {
-	int n;
-	FILE* file;
-};
+
 
 int main (int narg,char *argv[]){ 
 		int nProcesses;
 	if (narg >= 2)
 		nProcesses = atoi(argv[1]);
 	else
-		nProcesses = 100;
+		nProcesses = 0;
 	sem_close(sem_atend);
 	sem_unlink("/sem_atend");
 	sem_close(sem_block);
 	sem_unlink("/sem_block");
 	int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
-	std::cout << nProcesses << '\n';
-	std::cout << numCPU << "\n";
+	std::cout << "Avaliable Cores:  " << numCPU << "\n";
 	FILE* lng = fopen("lng.txt","w+");
 	struct reception_struct args;
 	args.n = nProcesses;
 	args.file = lng;
 	pthread_t thread1,thread2;
-	//pthread_create (&thread1,NULL,&reception,(void*)&args);
+	pthread_create (&thread1,NULL,&reception,(void*)&args);
+	pthread_join(thread1,NULL);
+	if ()
 	pthread_create (&thread2,NULL,&stop,NULL);
 	pthread_join(thread2,NULL);
 	return 0;
